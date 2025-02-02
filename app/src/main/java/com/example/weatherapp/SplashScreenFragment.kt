506b5dd2bd66f05aafe5478a925a1fefb4d54a86
Asma <asmaafreen26@gmail.com>
2025-02-03 00:31:45 +0530

    Extra Code

--- app/src/main/java/com/example/weatherapp/SplashScreenFragment.kt
+++ app/src/main/java/com/example/weatherapp/SplashScreenFragment.kt
@@ -1,5 +1,6 @@
 package com.example.weatherapp
 
+import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.VideoView
 import androidx.navigation.fragment.findNavController
 
 class SplashScreenFragment : Fragment() {
@@ -19,12 +21,20 @@ class SplashScreenFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_splash_screen, container, false)
     }
 
-        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-            super.onViewCreated(view, savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val videoView = view.findViewById<VideoView>(R.id.videoSplash)
 
-        Handler(Looper.getMainLooper()).postDelayed({
+        val videoUri = Uri.parse("android.resource://com.example.weatherapp/${R.raw.movingsun}")
+        videoView.setVideoURI(videoUri)
+        videoView.start()
+        videoView.setOnCompletionListener {
             findNavController().navigate(R.id.action_splashScreenFragment_to_weatherFragment)
-               },3000)
-
-            }
         }
+    Handler(Looper.getMainLooper()).postDelayed(
+    {
+        findNavController().navigate(R.id.action_splashScreenFragment_to_weatherFragment)
+    },8000)
+
+}
+}
